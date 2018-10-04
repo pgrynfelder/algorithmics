1753e9d31deddd2ec44ba67623f5f73a51e673cf
Piotr Grynfelder <piotr@grynfelder.dev>
2018-10-04 21:51:28 +0200

    tak

--- lekcje/papryka.cpp
+++ lekcje/papryka.cpp
@@ -7,22 +7,19 @@ int main(){
 	cin.tie(0);
 	cout.tie(0);
 
-	uint32_t hi = 0;
-	uint32_t k, a, current;
+	uint32_t hi = 0, k = 0, a = 0, pw = 1;
 
 	cin >> k;
-	for (uint32_t i = 0; i < k; i++){
+	for (uint32_t i = 0; i <= k; i++){
 		cin >> a;
-		current = 1;
-		for (uint32_t j = 0;j < i; j++){
-			current *= 2;
+		if (hi + 1 < pw){
+			cout << hi + 1 << '\n';
+			return 0;
 		}
-		current *= a;
-		if (hi > current + 1){
-			continue;
-		}
-		else {
-			cout << "";
-		}	
+		hi += pw*a;
+
+		pw *= 2;
 	}
+	cout << hi + 1 << '\n';
+	return 0;
 }
