be14fcea203eaf063df065d28dc4b523c83b8c61
Piotr Grynfelder <piotr@grynfelder.dev>
2018-10-06 14:03:43 +0200

    tak

---
+++ main/chomiki.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+uint32_t chomiki[7][500007];
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	uint32_t n, m;
+
+	cin >> n >> m;
+	return 0;
+}
