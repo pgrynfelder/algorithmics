544c1de0403311c351e339eeca199647a3b600c8
Piotr Grynfelder <piotr@grynfelder.dev>
2018-10-08 18:49:38 +0200

    yup

---
+++ kolko/mergesort.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+    uint32_t a;
+    cin >> a;
+    cout << a*a;
+
+	return 0;
+}
