ceb0858c823ad783622e164137cb3ce4d8bfca17
Piotr Grynfelder <piotr@grynfelder.dev>
2018-10-18 20:29:06 +0200

    xd :wq

--- kolko/przedzialprzedzial.cpp
+++ kolko/przedzialprzedzial.cpp
@@ -6,7 +6,6 @@ pair<bool, uint32_t>  T[2000007];
 uint32_t size=1;
 
 uint32_t check(uint32_t i, uint32_t l, uint32_t r, uint32_t a, uint32_t b){
-	if (i >= size) return 0;
 	
 	if (b < l or a > r){
 		return 0;
@@ -29,7 +28,7 @@ uint32_t check(uint32_t i, uint32_t l, uint32_t r, uint32_t a, uint32_t b){
 
 void add(uint32_t x, uint32_t i, uint32_t l, uint32_t r, uint32_t a, uint32_t b){
 	//cout << i <<' '<< l <<' '<< r <<' '<< a <<' '<< b << '\n';
-	if (b < l or a > r or i >= size){
+	if (b < l or a > r){
 	}
 	else if (a <= l and b >= r){
 		T[i].second = x;
@@ -37,9 +36,11 @@ void add(uint32_t x, uint32_t i, uint32_t l, uint32_t r, uint32_t a, uint32_t b)
 		//cout << "properly finished adding\n";
 	}
 	else {	
-		T[i].second = x;
+		//T[i].second = x;
 		add(x, i*2, l, (l+r)/2, a, b);
 		add(x, i*2+1, (l+r)/2+1, r, a, b);
+		T[i].second = max(T[i*2].second, T[i*2+1].second);
+		
 	}
 }
 
@@ -64,14 +65,14 @@ int main(){
 		add(lvl, 1, 1, size/2, b, a+b-1);
 	}
 	//cout << '\n';
-	/*for (uint32_t i = 1; i <= size; i *= 2){
+	for (uint32_t i = 1; i < size; i *= 2){
 		for (uint32_t j = i; j < i*2; j++){
 			cout << T[j].second;
 			if (T[j].first) cout << '*';
 			cout << ' ';
 		}
 		cout << '\n';
-	}*/
+	}
 	cout << T[1].second << '\n';
 	return 0;
 }
