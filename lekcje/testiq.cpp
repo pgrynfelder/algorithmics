97e37cb453717af064a355c2b3ac05c4c737bb43
Piotr Grynfelder <piotr@grynfelder.dev>
2018-12-06 22:21:04 +0100

    yeP

--- lekcje/testiq.cpp
+++ lekcje/testiq.cpp
@@ -1,17 +1,47 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// sortowanie inputu z zapamieaniem pozycji (pary warotsc, pozycja)
-// dla kazdego z podciagow
-// 	dla kazdego elementu
-// 		szukaj w posortowanym inpucie (binsearch)
-// 		jak znajdziesz, to ustaw na int max
-// 			jezeli pozycja < pozycja poprzedniego, to kontunuuj szukanie
-// 			jezeli >, to git i dawaj nastepny
-//
-// 			ale to jest kwadrat :///
+vector<int> xd[1000007];
 
-int main(){
-	return 0;
+int lbs(vector<int> &v, int x, int l, int r){
+	if (v.size() < 1 or x > v[v.size()-1]){
+		return -1;
+	}
+	while (l < r){
+		int mid = (l+r)/2;
+		if (x <= v[mid]){
+			r = mid;
+		}
+		else {
+			l = mid+1;
+		}
+	}
+	return l;
 }
 
+int main(){
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int m, n, p, a;
+	cin >> m;
+	for (int i = 0; i < m; i++){
+		cin >> a;
+		xd[a].push_back(i);
+	}
+	cin >> n;
+	for (int i = 0; i < n; i++){
+		cin >> p;
+		int current = 0;
+		for (int j = 0; j < p; j++){
+			cin >> a;
+			int x = lbs(xd[a], current, 0, xd[a].size()-1);
+			if (x == -1){
+				current = INT_MAX;
+			}
+			else {
+				current = xd[a][x] + 1;
+			}
+		}
+		cout << (current-1 <= m ? "TAK\n" : "NIE\n");
+	}
+	return 0;
+}
