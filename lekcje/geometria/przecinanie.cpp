7d6e3a02b1580ea3b9386d115961957b528ebf88
Piotr Grynfelder <piotr@grynfelder.dev>
2019-02-24 22:57:21 +0100

    yas

--- lekcje/geometria/przecinanie.cpp
+++ lekcje/geometria/przecinanie.cpp
@@ -5,16 +5,17 @@ using namespace std;
 typedef long long ll;
 
 bool crossed(ll ax, ll ay, ll bx, ll by, ll cx, ll cy, ll dx, ll dy){
-        int xd = 0;
+    int xd = 0;
 	// CD x CA i CD x CB	
-        if (((dx - cx)*(ay - cy) - (dy - cy)*(ax - cx)) * ((dx-cx)*(by-cy) - (dy-cy)*(bx-cx)) < 0){
-                xd++;
-        }
+	ll pro1 = ((dx - cx)*(ay - cy) - (dy - cy)*(ax - cx)) * ((dx-cx)*(by-cy) - (dy-cy)*(bx-cx));
+	cout << pro1 << "\n";
 	// AB x AD i AB x AC
-        if (((bx - ax)*(dy - ay) - (by - ay)*(dx - ax)) *  ((bx - ax)*(cy - ay) - (by - ay)*(cx - ax)) < 0){
-                xd++;
-        }
-        return xd == 2;
+	ll pro2 = ((bx - ax)*(dy - ay) - (by - ay)*(dx - ax)) *  ((bx - ax)*(cy - ay) - (by - ay)*(cx - ax));
+	cout << pro2 << "\n";
+
+	if(pro1 < 0 and pro2 < 0) return true;
+	if(pro1 == 0 and pro2 == 0) return true;
+	return false;
 }
 
 int main(){
@@ -23,6 +24,7 @@ int main(){
 	cout.tie(0);
         ll ax, ay, bx, by, cx, cy, dx, dy;
 	int n;
+	cin >> n;
 	while (n-->0){
 		cin >> ax >> ay >> bx >> by >> cx >> cy >> dx >> dy;
 		if (crossed(ax,ay,bx,by,cx,cy,dx,dy)){
