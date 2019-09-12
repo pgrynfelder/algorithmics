f55461c6c6bc0713ab6eee1a6253f53f347495f2
Piotr Grynfelder <piotr@grynfelder.dev>
2019-09-12 20:08:22 +0200

    x

---
+++ lekcje/dynamiki/gornicy.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// DP[posilek 1 1][posilek 1 2][posilek 2 1][posilek 2 2]
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    return 0;
+}
+
