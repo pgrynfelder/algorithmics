299b74b033a0bb79d41112b881f2d99b757e6947
Piotr Grynfelder <piotr@grynfelder.dev>
2019-09-19 21:03:01 +0200

    yeses

--- lekcje/plecak/sredniaarytmetyczna.cpp
+++ lekcje/plecak/sredniaarytmetyczna.cpp
@@ -1,43 +1,57 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int n, a;
 const int N = 507;
-const int M = 2*N*N;
+const int M = 2 * N * N;
 // const int M = 20;
-const int C = M/2 - 2;
+const int C = M / 2 - 2;
 
 int numbers[N];
 bool DP[N][M];
-int main(){
+int main()
+{
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
     cin >> n >> a;
-    for (int i = 1; i <= n; i++){
+    for (int i = 1; i <= n; i++)
+    {
         cin >> numbers[i];
         numbers[i] -= a;
     }
-    
+    int rng = 2 * n * N;
     queue<int> result;
     DP[1][C + numbers[1]] = true;
-    for (int i = 2; i <= n; i++){
-        for (int j = 0; j < M; j++){
-            DP[i][j] = DP[i-1][j];
-            if (j-numbers[i] < M and j - numbers[i] >= 0)
-                DP[i][j] = DP[i][j] or DP[i-1][j-numbers[i]];
+    if (DP[1][C])
+    {
+        cout << "TAK\n1\n"
+             << numbers[1] + a << ' ';
+        return 0;
+    }
+    for (int i = 2; i <= n; i++)
+    {
+        for (int j = C - rng; j < C + rng; j++)
+        {
+            DP[i][j] = DP[i - 1][j];
+            if (j - numbers[i] < M and j - numbers[i] >= 0)
+                DP[i][j] = DP[i][j] or DP[i - 1][j - numbers[i]];
         }
-        if (DP[i][C]){
+        if (DP[i][C])
+        {
             cout << "TAK\n";
             int j = C;
-            while (i > 0){
-                if (DP[i-1][j] != true) {
+            while (i > 0)
+            {
+                if (DP[i - 1][j] != true)
+                {
                     j -= numbers[i];
                     result.push(numbers[i]);
                 }
                 i--;
             }
             cout << result.size() << '\n';
-            while (!result.empty()){
+            while (!result.empty())
+            {
                 cout << result.front() + a << ' ';
                 result.pop();
             }
