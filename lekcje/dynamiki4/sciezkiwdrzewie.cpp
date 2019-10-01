682d7f649a816da8f59dfea2ede44bdb2ec7fdb1
Piotr Grynfelder <piotr@grynfelder.dev>
2019-10-01 23:39:31 +0200

    WA on 10

--- lekcje/dynamiki4/sciezkiwdrzewie.cpp
+++ lekcje/dynamiki4/sciezkiwdrzewie.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-const int N = 2e6+7;
+const int N = 2e5+7;
 int n, k;
 long long result;
 
@@ -9,45 +9,60 @@ struct Counter {
     int c = 0;
     unordered_map<int, int> values;
     void add(int i, int x){
-        values[i - c]+=x;
+        values[i - c] += x;
     }
     int get(int i){
-       return values[i - c];
+        if (values.count(i-c))
+            return values[i - c];
+        return 0;
     }
     size_t size(){ return values.size(); }
+    void print(){
+        for (auto x : values){
+            cout << x.first + c << ": " << x.second << "\n";
+        }
+    }
 };
 
 Counter *counter[N];
-
 vector<int> G[N];
 
 void dfs(int v, int p){
-    Counter *biggust = 0;
+    Counter *biggest = 0;
     for (int u : G[v]){
         if (u != p){
             dfs(u, v);
-            if (!biggust or counter[u]->size() > biggust->size()){
-                biggust = counter[u];
+            if (!biggest or counter[u]->size() > biggest->size()){
+                biggest = counter[u];
             }
         }
     }
-    counter[v] = biggust;
+    counter[v] = biggest;
     if (!counter[v]){
         counter[v] = new Counter();
+        // cout << v << " created new counter\n";
     }
     counter[v]->c++;
     for (int u : G[v]){
-        if (u != p and counter[v] != counter[u]){
-            for (auto x : counter[u]->values){
-                int len = x.first + counter[u]->c;
-                if (len < k - 1){
-                    result += x.second * counter[v]->get(k - len);
-                }
-                counter[v]->add(len, x.second);
+        if (u != p){
+            if (counter[v] == counter[u]){
+                // cout << v << " took counter from " << u << "\n";
             }
+            else {
+                for (auto x : counter[u]->values){
+                    int len = x.first + counter[u]->c;
+                    if (len < k - 1){
+                        // cout << "added paths going through root +" << x.second * counter[v]->get(k - len) << "\n";
+                        result += x.second * counter[v]->get(k - len);
+                    }
+                    counter[v]->add(len+1, x.second);
+                }
+            }         
         }
     }
     counter[v] -> add(1, 1);
+    // counter[v] -> print();
+
     result += counter[v]->get(k);
 }
 
