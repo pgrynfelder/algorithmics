63ae6d06aeafa0fa90ce501a1bd3d1ffe8ff11d6
Piotr Grynfelder <piotr@grynfelder.dev>
2019-11-01 01:03:36 +0100

    pis neural network should work now

--- oi/pis/main.py
+++ oi/pis/main.py
@@ -2,73 +2,73 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import numpy as np
 
+import sklearn.utils as skl
 import tensorflow as tf
 
-import tensorflow_hub as hub
-import tensorflow_datasets as tfds
-
-sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
-tf.enable_eager_execution()
+# tf.enable_eager_execution()
 tf.keras.backend.clear_session()
 
 print("Version: ", tf.__version__)
 print("Eager mode: ", tf.executing_eagerly())
-print("Hub version: ", hub.__version__)
 print("GPU is", "available" if tf.config.experimental.list_physical_devices("GPU") else "NOT AVAILABLE")
 
 import testing
 import preprocessing as pp
 # testing.generate()
-tests = [1,2,3,4]
+tests = [1, 2, 3, 4]
 titles = dict(Sienkiewicz = np.array([0,0,1]), Mickiewicz = np.array([1,0,0]), Prus = np.array([0,1,0]))
+
+examples = []
+labels = []
+
 for n in tests:
-    train_examples = []
-    train_labels = []
-    for j in range(0, 100):
+    for j in range(0, 10):
         j = str(j)
         j = j.zfill(3)
         with open(f"tests/{n}{j}.in") as test:
             for i, line in enumerate(test.readlines()):
-                if i != 0: train_examples.append(
+                if i != 0: examples.append(
                     np.array(
                     pp.resize(
                     pp.hash(
                     pp.stem(
                     pp.depunctuate(
-                    line.lower())))), dtype=np.float64))
+                    line.lower())))), dtype=np.float32))
 
         with open(f"tests/{n}{j}.out") as result: 
             for a in result.readlines():
-                train_labels.append(titles[a.replace('\n', '')])
-            
-train_examples = np.array(train_examples, np.float64)
-train_labels = np.array(train_labels, np.float64)
+                labels.append(titles[a.replace('\n', '')])
 
-DATASET_SIZE = len(train_examples)
+DATASET_SIZE = len(examples)
+examples, labels = skl.shuffle(examples, labels, random_state=0)
 
-print("Data preparaton finished")
+train_examples = np.array(examples[:DATASET_SIZE//2], np.float32)
+train_labels = np.array(labels[:DATASET_SIZE//2], np.float32)
+val_examples = np.array(examples[DATASET_SIZE//2:], np.float32)
+val_labels = np.array(labels[DATASET_SIZE//2:], np.float32)
 
-with tf.device('/GPU:0'):
+print("Data preparation finished")
 
-    ds = tf.data.Dataset.from_tensor_slices((train_examples, train_labels))
-
-    ds = ds.shuffle(DATASET_SIZE).batch(10)
-
-    train = ds.take(DATASET_SIZE//20)
-    val = ds.skip(DATASET_SIZE//20)
-    model = tf.keras.Sequential([
+# ds = tf.data.Dataset.from_tensor_slices((train_examples, train_labels))
+# # ds = tf.data.Dataset((train_examples, train_labels))
+# ds = ds.shuffle(DATASET_SIZE).batch(1)
+#
+# train = ds.take(DATASET_SIZE//2)
+# val = ds.skip(DATASET_SIZE//2)
+model = tf.keras.Sequential([
     tf.keras.layers.Dense(100, activation="sigmoid"),
     tf.keras.layers.Dense(20, activation="sigmoid"),
     tf.keras.layers.Dense(20, activation="sigmoid"),
+    tf.keras.layers.Dense(20, activation="sigmoid"),
     tf.keras.layers.Dense(3, activation="softmax")
-    ])
+])
 
-    model.compile(optimizer='adam',
-                # loss='sparse_categorical_crossentropy',
-                loss=tf.keras.losses.MeanSquaredError(),
-                metrics=['accuracy'])
+model.compile(optimizer='adam',
+            # loss='sparse_categorical_crossentropy',
+            loss=tf.keras.losses.MeanSquaredError(),
+            metrics=['accuracy'])
 
-    model.fit(train, epochs=1000, validation_data=val)
+model.fit(train_examples, train_labels, epochs=1000, validation_data=(val_examples, val_labels))
 
-    model.summary()
-    model.save('my_model.h5')
+model.summary()
+model.save('my_model.h5')
--- oi/pis/testing.py
+++ oi/pis/testing.py
@@ -4,10 +4,10 @@ def generate():
     os.system("mkdir tests")
     os.chdir("tests")
     for i in range(1, 5):
-        for j in range(0, 1000):
+        for j in range(0, 100):
             j = str(j)
             j = j.zfill(3)
-            os.system(f"python3 ../files/pistestgen.py {i} {i}{j} ../files")
+            os.system(f"python ../files/pistestgen.py {i} {i}{j} ../files")
         
 if __name__ == "__main__":
     generate()
