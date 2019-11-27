3a1b5dfc23688a53e22e7f85136c01f313dcaa8f
Piotr Grynfelder <piotr@grynfelder.dev>
2019-11-27 09:59:11 +0100

    still not working

--- lekcje/struktury/odcinki2.cpp
+++ lekcje/struktury/odcinki2.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 template<typename T>
-struct Tree {
+struct sumtree {
     int n = 1;
     vector<T> elements;
     Tree(int _n){
@@ -49,39 +49,120 @@ struct Tree {
          }
     }
 };
-// the tree's indexed from 0 to n-1
 
+template<typename T>
+struct Sumtree {
+    int n = 1;
+    vector<T> elements;
+    Sumtree(int _n){
+        while (n < _n){
+            n <<= 1;
+        }
+        n <<= 1;
+        elements.resize(n);
+        n >>= 1;
+    }
+    void insert(int i, T x){
+        i += n;
+        elements[i] = x;
+        while (i > 1){
+            i >>= 1;
+            elements[i] = elements[2*i] + elements[2*i+1];
+        }
+    }
+    T _read(int a, int b, int l, int r, int i){
+        if (a <= l and r <= b){
+            return elements[i];
+        }
+        int mid = (l+r)/2;
+        T result = 0;
+        if (a <= mid) result += _read(a, b, l, mid, 2*i);
+        if (b >= mid + 1) result += _read(a, b, mid + 1, r, 2*i + 1);
+        return result;
+    }
+    T read(int a, int b){
+        if (b < a) return 0;
+        return _read(a, b, 0, n-1, 1);
+    }
+    void print(){
+         int i = 1;
+         int m = 1;
+         int lim = n*2;
+         while (i < lim){
+            while (i < m){
+                cout << elements[i] << ' ';
+                i++;
+            }
+            m *= 2;
+            cout << "\n";
+         }
+    }
+};
+
+
+template<typename T>
+struct Tree {
+    int n = 1;
+    vector<T> elements;
+    Tree(int _n){
+        while (n < _n){
+            n <<= 1;
+        }
+        n <<= 1;
+        elements.resize(n);
+        n >>= 1;
+    }
+    void insert(int i, T x){
+        i += n;
+        elements[i] = x;
+        while (i > 1){
+            i >>= 1;
+            elements[i] = elements[2*i] + elements[2*i+1];
+        }
+    }
+    T _read(int a, int b, int l, int r, int i){
+        if (a <= l and r <= b){
+        //    return elements[i];
+        }
+        int mid = (l+r)/2;
+        T result = 0;
+        if (a <= mid) // result += _read(a, b, l, mid, 2*i);
+        if (b >= mid + 1) // result += _read(a, b, mid + 1, r, 2*i + 1);
+        return result;
+    }
+    T read(int a, int b){
+        if (b < a) return 0;
+        return _read(a, b, 0, n-1, 1);
+    }
+    void print(){
+         int i = 1;
+         int m = 1;
+         int lim = n*2;
+         while (i < lim){
+            while (i < m){
+                cout << elements[i] << ' ';
+                i++;
+            }
+            m *= 2;
+            cout << "\n";
+         }
+    }
+};
 int n, m;
 
 int main(){
     ios_base::sync_with_stdio(0); cin.tie(0);
     cin >> n >> m;
-    Tree<int> t(n);
-    long long xd = 0;
-    string s; cin >> s;
+    Sumtree<int> s(n);
+    result<int> res(n);
+    string seq; cin >> seq;
     for (int i = 0; i < n; i++){
-        if (s[i] == 'K'){
+        if (s[i] == 'P'){
             t.insert(i, 1);
-            xd += i - t.read(0, i-1); 
         } 
     }
     cout << xd << "\n";
-    while (m--){
-        char c;
-        cin >> c;
-        if (c == 'X'){
-            int i; cin >> i;
-            i--;
-            if (t.read(i, i) == 0){
-                t.insert(i, 1);
-            }
-            else {
-                t.insert(i, 0);
-            }
-        }
-        else {
-            int a, b; cin >> a >> b;
-            a--; b--;
-    }
+    
+    
     return 0;
 }
