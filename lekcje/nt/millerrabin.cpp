2332e9b4f2fbba25b4329eaf08d86c73022165bc
Piotr Grynfelder <piotr@grynfelder.dev>
2019-11-28 22:23:22 +0100

    wielomian <3

--- lekcje/nt/millerrabin.cpp
+++ lekcje/nt/millerrabin.cpp
@@ -1,8 +1,10 @@
+// miller rabin prime
 #include<bits/stdc++.h>
 using namespace std;
 typedef unsigned long long ull;
 
 constexpr int K = 36, L = 2, R = 37;
+ull primes[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37};
 
 ull trailing_zeros(ull x){
     ull count = 0;
@@ -13,38 +15,61 @@ ull trailing_zeros(ull x){
     return count;
 }
 
-ull p(ull m, ull n, ull mod){ 
-    ull res = 1;
+ull mul(ull m, ull n, ull mod){ 
+    ull res = 0;
     while (n > 0) {
         if(n & 1) {
-            res = res * m % mod;
+            res = (res + m) % mod;
         }
-        m = m * m % mod;
+        m = (m + m) % mod;
         n >>= 1;
     }
     return res;
 }
+
+ull p(ull m, ull n, ull mod){ 
+    ull res = 1;
+    while (n > 0) {
+        if(n & 1) {
+            res = mul(m, res, mod);
+        }
+        m = mul(m, m, mod);
+        n >>= 1;
+    }
+    return res;
+}
+
+
 // jeden rabin powie tak a drugi powie nie 
 bool rabin(ull n){
     if (n==1) return false;
     else if (n==2) return true;
     else if (n==3) return true;
-    ull s = trailing_zeros(n);
-    ull d = n / (1 << s);
-    cout << s << " " << d << "\n";
-    for (int a = L; a <= R; a++){
+    ull s = trailing_zeros(n-1);
+    ull d = (n-1) / (1 << s);
+    // cout << "n = 2^" << s << " * " << d << " + 1\n";
+    // cout << s << " " << d << "\n";
+    for (ull a : primes){
+        if (a > n-2) break;
         ull x = p(a, d, n);
+        
         bool composite = true;
-        if (x % n != 1){
-            composite = false;
+        if (x == 1){
+            // cout << x << "\n";
+            continue;
         }
-        for (int i = 0; i < s; i++){
-            if (x % n != n-1){
+        for (ull i = 0; i < s; i++){
+            // cout << x << "\n";
+            if (x == n-1){
                 composite = false;
             }
-            x = x * x % n; 
+            x = mul(x, x, n); 
         }
-        if (composite) return false;
+        
+        if (composite){
+            // cout << "wypitol for x=" << x << "\n";
+            return false;
+        }
     }
     return true;
 }
@@ -52,8 +77,13 @@ bool rabin(ull n){
     
     
 int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     ull n;
     cin >> n;
-    cout << rabin(n);   
+    while (n--){
+        ull x;
+        cin >> x;
+        cout << (rabin(x) ? "TAK" : "NIE") << "\n";   
+    }
     return 0;
 }
---
+++ lekcje/nt/wielomian.cpp
@@ -0,0 +1,88 @@
+// wielomian polynomial
+#include<bits/stdc++.h>
+using namespace std;
+typedef unsigned long long ull;
+
+constexpr int MAX_N = 1007, MAX_M = 1007;
+ull n, mod;
+vector<vector<ull>> es;
+vector<ull> sol;
+
+ull p(ull m, ull n, ull mod){ 
+    ull res = 1;
+    while (n > 0) {
+        if(n & 1) {
+            res = m * res % mod;
+        }
+        m = m * m % mod;
+        n >>= 1;
+    }
+    return res;
+}
+
+ull inv(ull n, ull mod){
+    return p(n, mod-2, mod);
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> n >> mod;
+    n++;
+    es = vector<vector<ull>>(n, vector<ull>(n+1));
+    sol = vector<ull>(n);
+    for (int i = 0; i < n; i++){
+        ull x, y;
+        cin >> x >> y;
+        x %= mod; y %= mod;
+        ull res = 1;
+        for (int j = 0; j < n; j++){
+            es[i][j] = res;
+            res = res * x % mod;
+        }
+        es[i][n] = y;
+    }
+    
+    for (int x = 0; x < n; x++){
+        for (int i = x; i < n; i++){
+            if (es[i][x] > es[x][x]){
+                swap(es[i], es[x]);
+            }
+        }
+        for (int y = x+1; y < n; y++){
+            if (es[y][x] != 0){
+                ull k = es[y][x] * inv(es[x][x], mod) % mod;
+                for (int j = x+1; j < n + 1; j++){
+                    es[y][j] = (mod + es[y][j] - es[x][j] * k % mod) % mod;
+                }
+                es[y][x] = 0;
+            }
+        }
+    }
+    for (int x = n-1; x >= 0; x--){
+        int i = 0;
+        while (i < n){
+            if (es[x][i] != 0){
+                break;
+            }
+            i++;
+        }
+        // cout << i << "\n"; 
+        if (i == n and es[x][i] != 0){
+            cout << "sprzeczny\n"; return 0;
+        }
+        else if (i == n and es[x][i] == 0){
+            continue;
+        }
+        else {
+            sol[i] = es[x][n];
+            for (int j = i + 1; j < n; j++){
+                sol[i] = (mod + sol[i] - sol[j] * es[x][j] % mod) % mod;
+            }
+            sol[i] = sol[i] * inv(es[x][i], mod) % mod;
+        }
+    }
+    for (int i = 0; i < n; i++){
+        cout << sol[i] << "\n";
+    }
+    return 0;      
+}
