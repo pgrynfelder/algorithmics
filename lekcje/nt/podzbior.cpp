8b52c9185d7c219dc63bb5179b8822802d74dcbc
Piotr Grynfelder <piotr@grynfelder.dev>
2019-11-28 23:59:54 +0100

    podzbior almost

--- lekcje/nt/podzbior.cpp
+++ lekcje/nt/podzbior.cpp
@@ -3,62 +3,41 @@
 using namespace std;
 typedef unsigned long long ull;
 
-constexpr int MAX_N = 1007, MAX_M = 1007;
-ull n, mod;
-vector<vector<ull>> es;
-vector<ull> sol;
+constexpr int m = 64;
+int n, q;
+vector<vector<int>> es_base;
+vector<int> sol;
 
-ull p(ull m, ull n, ull mod){ 
-    ull res = 1;
-    while (n > 0) {
-        if(n & 1) {
-            res = m * res % mod;
-        }
-        m = m * m % mod;
-        n >>= 1;
+int gauss(ull d){
+    vector<vector<int>> es = es_base;
+    for (int j = 0; j < m; j++){
+        es[j][n] = d & 1;
+        d >>= 1;
     }
-    return res;
-}
 
-ull inv(ull n, ull mod){
-    return p(n, mod-2, mod);
-}
-
-int main(){
-    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    cin >> n >> mod;
-    n++;
-    es = vector<vector<ull>>(n, vector<ull>(n+1));
-    sol = vector<ull>(n);
-    for (int i = 0; i < n; i++){
-        ull x, y;
-        cin >> x >> y;
-        x %= mod; y %= mod;
-        ull res = 1;
-        for (int j = 0; j < n; j++){
-            es[i][j] = res;
-            res = res * x % mod;
-        }
-        es[i][n] = y;
-    }
-    
     for (int x = 0; x < n; x++){
-        for (int i = x; i < n; i++){
+        for (int i = x; i < m; i++){
             if (es[i][x] > es[x][x]){
                 swap(es[i], es[x]);
             }
         }
-        for (int y = x+1; y < n; y++){
-            if (es[y][x] != 0){
-                ull k = es[y][x] * inv(es[x][x], mod) % mod;
+        for (int y = x+1; y < m; y++){
+            if (es[y][x] == 1 and es[x][x] == 1){
                 for (int j = x+1; j < n + 1; j++){
-                    es[y][j] = (mod + es[y][j] - es[x][j] * k % mod) % mod;
+                    es[y][j] ^= es[x][j];
                 }
                 es[y][x] = 0;
             }
         }
     }
-    for (int x = n-1; x >= 0; x--){
+    // for (int x = 0; x < m; x++){
+    //     for (int i = 0; i < n + 1; i++){
+    //         cout << es[x][i] << " ";
+    //     }
+    //     cout << "\n";
+    // }
+
+    for (int x = m-1; x >= 0; x--){
         int i = 0;
         while (i < n){
             if (es[x][i] != 0){
@@ -68,7 +47,7 @@ int main(){
         }
         // cout << i << "\n"; 
         if (i == n and es[x][i] != 0){
-            cout << "sprzeczny\n"; return 0;
+            return 0;
         }
         else if (i == n and es[x][i] == 0){
             continue;
@@ -76,13 +55,32 @@ int main(){
         else {
             sol[i] = es[x][n];
             for (int j = i + 1; j < n; j++){
-                sol[i] = (mod + sol[i] - sol[j] * es[x][j] % mod) % mod;
+                sol[i] ^= (sol[j] & es[x][j]);
             }
-            sol[i] = sol[i] * inv(es[x][i], mod) % mod;
         }
     }
+    return 1;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> n >> q;
+    es_base = vector<vector<int>>(m, vector<int>(n+1));
+    sol = vector<int>(n);
     for (int i = 0; i < n; i++){
-        cout << sol[i] << "\n";
+        ull a; cin >> a;
+        for (int j = 0; j < m; j++){
+            es_base[j][i] = a & 1;
+            a >>= 1;
+        }
+    }
+    // input
+    while (q--){
+        // cout << "q: " << q << "\n";
+        ull d;
+        cin >> d;
+        cout << (gauss(d) ? "TAK\n" : "NIE\n");
+        
     }
     return 0;      
 }
