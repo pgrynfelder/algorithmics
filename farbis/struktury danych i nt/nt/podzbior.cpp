86d4528a026c5cc5b5e46e9381f0568c6c7915f5
Piotr Grynfelder <piotr@grynfelder.dev>
2021-01-12 12:11:04 +0100

    farbiś archive fixes

--- farbis/struktury danych i nt/nt/podzbior.cpp
+++ farbis/struktury danych i nt/nt/podzbior.cpp
@@ -3,101 +3,33 @@
 using namespace std;
 typedef unsigned long long ull;
 
-constexpr int m = 64;
-int n, q;
-vector<vector<int>> es_base;
-vector<int> sol;
-vector<ull> numbers;
-
-int gauss(ull _d){
-    ull d = _d;
-    vector<vector<int>> es = es_base;
-    vector<int> expected(m);
-    for (int j = 0; j < m; j++){
-        expected[j] = es[j][n] = d & 1;
-        d >>= 1;
-    }
-
-    vector<int> where(n, -1);
-    for (int col = 0, row = 0; col < n; col++){
-        for (int y = row + 1; y < m; y++){
-            if (es[y][col] > es[row][col]){
-                swap(es[y], es[row]);
-            }
-        }
-        if (es[row][col] == 0){
-            continue;
-        }
-        where[col] = row;
-        for (int y = row + 1; y < m; y++){
-            if (es[y][col] == 1){
-                for (int x = col; x <= n; x++){
-                    es[y][x] ^= es[row][x];
-                }
-            }
-        }
-        row++;    
-    }
-    //for (int x = 0; x < m; x++){
-    //    for (int i = 0; i < n + 1; i++){
-    //        cout << es[x][i] << " ";
-    //    }
-    //    cout << "\n";
-    //}
-
-    for (int col = n-1; col >= 0; col--){
-        if (where[col] == -1){
-            sol[col] = 0;
-        }
-        else {
-            int row = where[col];
-            sol[col] = es[row][n];
-            for (int x = col+1; x < n; x++){
-                sol[col] ^= es[row][x] & sol[x];
-            }
-
-        }
-    }
-    // double checker
-    for (int x = 0; x < m; x++){
-        int res = 0;
-        for (int i = 0; i < n; i++){
-            // cout << sol[i] << ";" << es_base[x][i] << " ";
-            res ^= sol[i] & es_base[x][i];
-        }
-        // cout << res << "=" << expected[x] << "\n";
-        if (res != expected[x]) {
-            //for (int i = 0; i < n; i++){
-            //    cout << numbers[i] << ";";
-            //}
-            //cout << _d << "\n";
-            return 0;
-        }
-    }
-    return 1;
-}
-
 int main(){
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    ull n, q;
     cin >> n >> q;
-    es_base = vector<vector<int>>(m, vector<int>(n+1));
-    sol = vector<int>(n);
-    numbers = vector<ull>(n);
-    for (int i = 0; i < n; i++){
-        ull a; cin >> a;
-        numbers[i] = a;
-        for (int j = 0; j < m; j++){
-            es_base[j][i] = a & 1;
-            a >>= 1;
+    
+    set<ull, greater<ull>> xd;
+    while (n--){
+        ull a;
+        cin >> a;
+        for (ull b : xd){
+            // cout << "y:" << y <<" ";
+            a = min(a, a^b);
         }
-    }
-    // input
-    while (q--){
-        // cout << "q: " << q << "\n";
-        ull d;
-        cin >> d;
-        cout << (gauss(d) ? "TAK\n" : "NIE\n");
         
+        if (a != 0) xd.insert(a);   
+    }
+    while (q--){
+        ull a;
+        cin >> a;
+        for (ull b : xd){
+            a = min(a, a^b);
+        }
+        if (a == 0){
+            cout << "TAK\n";
+        } else {
+            cout << "NIE\n";
+        }
     }
     return 0;      
 }
