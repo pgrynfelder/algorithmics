d717127cee68b9bec60202a126ab16625d0056b0
Piotr Grynfelder <piotr@grynfelder.dev>
2021-02-05 22:21:29 +0100

    pla 81

--- preoi2021/day10/pla1/a.cpp
+++ preoi2021/day10/pla1/a.cpp
@@ -3,59 +3,44 @@ using namespace std;
 
 constexpr int N = 1e5 + 7;
 int n, q, H;
-constexpr pair<int, int> def = {-1, 0};
 int l[N], r[N], h[N];
 
-map<int, int> scale = {{0, 0}};
+map<int, int> scale;
 int X;
 
-// template <typename T, typename U>
-// std::pair<T, U> operator+(const std::pair<T, U>& l, const std::pair<T, U>& r)
-// {
-//     return {l.first + r.first, l.second + r.second};
-// }
-
+constexpr pair<int, int> def = {-1, 0};
 template <typename T>
 struct segsegtree {
     int n = 1;
     vector<T> t;
-    vector<T> prop;
     vector<bool> star;
     segsegtree(int _n) {
         while (n < _n) {
-            n <<= 1;
+            n *= 2;
         }
-        n <<= 1;
-        t.assign(n, def);
-        prop.resize(n, def);
-        star.resize(n, false);
-        n >>= 1;
+        t.assign(2 * n, def);
+        star.resize(2 * n, false);
     }
     void propagate(int i) {
-        if (i < n and star[i]) {
-            t[2 * i] = t[2 * i + 1] = prop[2 * i] = prop[2 * i + 1] = prop[i];
+        if (star[i]) {
+            t[2 * i] = t[2 * i + 1] = t[i];
             star[2 * i] = star[2 * i + 1] = true;
             star[i] = false;
         }
     }
-    void update(int i) {
-        if (i < n) {
-            t[i] = max(t[2 * i], t[2 * i + 1]);
-        }
-    }
+    void update(int i) { t[i] = max(t[2 * i], t[2 * i + 1]); }
+
     int a, b;
     T x;
     T _read(int l, int r, int i) {
         if (a <= l and r <= b) {
             return t[i];
         }
-        int mid = (l + r) / 2;
+        int m = l + r >> 1;
         T result = def;
-        if (a <= mid or mid + 1 <= b) {
-            propagate(i);
-        }
-        if (a <= mid) result = max(result, _read(l, mid, 2 * i));
-        if (mid + 1 <= b) result = max(result, _read(mid + 1, r, 2 * i + 1));
+        propagate(i);
+        if (a <= m) result = max(result, _read(l, m, 2 * i));
+        if (m < b) result = max(result, _read(m + 1, r, 2 * i + 1));
         return result;
     }
     T read(int _a, int _b) {
@@ -65,17 +50,13 @@ struct segsegtree {
     void _insert(int l, int r, int i) {
         if (a <= l and r <= b) {
             t[i] = x;
-            prop[i] = x;
             star[i] = true;
             return;
         }
-        int mid = (l + r) / 2;
-        if (a <= mid or mid + 1 <= b) {
-            propagate(i);
-        }
-        if (a <= mid) _insert(l, mid, 2 * i);
-        if (mid + 1 <= b) _insert(mid + 1, r, 2 * i + 1);
-
+        int m = l + r >> 1;
+        propagate(i);
+        if (a <= m) _insert(l, m, 2 * i);
+        if (m < b) _insert(m + 1, r, 2 * i + 1);
         update(i);
     }
     void insert(int _a, int _b, T _x) {
@@ -110,20 +91,17 @@ int main() {
     cout.tie(0);
 
     cin >> n >> q >> H;
-    set<int> xs;
     for (int i = 1; i <= n; i++) {
         int x, y, len;
         cin >> x >> y >> len;
         l[i] = x;
         r[i] = x + len - 1;
         h[i] = y;
-        xs.emplace(l[i]);
-        xs.emplace(r[i]);
+        scale[l[i]] = scale[r[i]] = 0;
     }
-    for (int x : xs) {
-        scale[x] = scale.rbegin()->second + 1;
+    for (auto it = ++scale.begin(); it != scale.end(); it++) {
+        it->second = prev(it)->second + 1;
     }
-    xs.clear();
     X = scale.size();
 
     vector<int> platforms;
@@ -133,10 +111,11 @@ int main() {
         platforms.emplace_back(i);
         fu[i] = i;
     }
-    sort(platforms.begin(), platforms.end(),
-         [](int a, int b) { return h[a] < h[b]; });
 
-    segsegtree<pair<int, int>> t(X + 1);
+    sort(platforms.begin(), platforms.end(),
+         [](const int a, const int b) { return h[a] < h[b]; });
+
+    segsegtree<pair<int, int>> t(X);
 
     for (int a : platforms) {
         auto xd = t.read(l[a], r[a]);
