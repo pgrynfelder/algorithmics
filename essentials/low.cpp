d7d9314597fa58cdb2390682e1acd846690aef67
Piotr Grynfelder <piotr@grynfelder.dev>
2021-04-03 16:40:44 +0200

    low rewrite

--- essentials/low.cpp
+++ essentials/low.cpp
@@ -13,32 +13,24 @@ vector<int> g[N];
 int dist[N], low[N];
 
 bool visited[N];
-int p[N];
-void dfs(int a) {
+void bridge(int a, int b);
+void cutpoint(int a);
+void dfs(int a, int p = -1) {
     low[a] = dist[a];
     for (int b : g[a]) {
-        if (b != p[a]) {
-            low[a] = min(low[a], dist[b]);
-        }
+        if (b == p) continue;
+
+        low[a] = min(low[a], dist[b]);
 
         if (!visited[b]) {
             visited[b] = true;
             dist[b] = dist[a] + 1;
-            p[b] = a;
-            dfs(b);
+            dfs(b, a);
             low[a] = min(low[a], low[b]);
         }
+        if (dist[a] < low[b]) bridge(a, b);
+        if (dist[a] <= low[b] and p != -1) cutpoint(a);
     }
-}
-
-bool is_bridge(int a, int b) { return dist[a] < low[b]; }
-
-bool is_articulation_point(int a) {
-    if (a == 1 and g[a].size() > 1) return true;
-    if (a != 1)
-        for (int b : g[a])
-            if (dist[a] <= low[b] and b != p[a]) return true;
-    return false;
 }
 
 int main() {
